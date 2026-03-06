8081cbca20b330041d2e6ba3fd4c7c61b2db7587
gabriel souza <gabrielsouza_1909@hotmail.com>
2026-03-06 12:23:39 -0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/taskboard/",
   plugins: [react()],
   resolve: {
     alias: {
-      "react-native": "react-native-web"
-    }
-  }
-})
+      "react-native": "react-native-web",
+    },
+  },
+});
